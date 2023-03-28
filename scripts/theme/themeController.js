16b8137997068e506db0acc1ad542e6ae78881ec
Vinay Kumar Malik <107244657+vinay21387@users.noreply.github.com>
2023-03-28 11:35:19 +0530

    Create themeController.js

---
+++ scripts/theme/themeController.js
@@ -0,0 +1,34 @@
+export function createThemeController(){
+
+    updateTheme()
+
+    function switchTheme(){
+        document.body.classList.toggle("dark");
+        if (themeIsDark()) 
+            set("light")
+        else 
+            set("dark")
+    
+        updateTheme()
+    }
+    
+    function updateTheme(){
+        if(themeIsDark())
+            document.body.classList.toggle("dark", true) 
+    }
+    
+    function themeIsDark(){
+        return get() === "dark"
+    }
+    
+    function set(theme){
+        window.localStorage.setItem("theme", theme)
+    }
+    function get(){
+        return window.localStorage.getItem("theme")
+    }
+
+    return {
+        switchTheme
+    }
+}
