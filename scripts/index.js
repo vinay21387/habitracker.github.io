e33a501938ae4c178baa6cf4940c3a4aae0d2dd2
Vinay Kumar Malik <107244657+vinay21387@users.noreply.github.com>
2023-03-28 11:29:18 +0530

    Create index.js

---
+++ scripts/index.js
@@ -0,0 +1,48 @@
+import { dayButtons, 
+         bttnPreviousHabitMonth, 
+         bttnNextHabitMonth, 
+         bttnTrashCan, 
+         bttnDarkModeToggle }         from './utils/DOMelements.js';
+
+
+import { createThemeController }      from './theme/themeController.js';
+import { createHabitMonthController } from './habit-month/HabitMonthController.js';
+import { createHabitMonthView }       from './habit-month/HabitMonthView.js';
+import { createHabitMonthStreakView } from './habit-month/HabitMonthStreakView.js';
+
+
+const themeController = createThemeController()
+
+const habitMonthView       = createHabitMonthView()
+const habitMonthStreakView = createHabitMonthStreakView()
+const habitMonthController = createHabitMonthController()
+
+habitMonthController.subscribe(habitMonthView.update)
+habitMonthController.subscribe(habitMonthStreakView.update)
+
+habitMonthController.changeToDefault()
+
+// EVENT LISTENERS:
+
+bttnPreviousHabitMonth.addEventListener("click", ()=>{
+    habitMonthController.changeToPrevious()
+})
+bttnNextHabitMonth.addEventListener("click", ()=>{
+    habitMonthController.changeToNext()
+})
+
+dayButtons.forEach( button =>{
+    button.addEventListener("click", ()=>{
+        const dayIndex = parseInt(button.innerText) - 1
+        habitMonthController.switchDayState(dayIndex)
+    })
+})
+
+bttnTrashCan.addEventListener("click", ()=>{
+    if (confirm("You sure? This action can't be undone!"))
+        habitMonthController.clearAllDataFromViewingMonth()  
+})
+
+bttnDarkModeToggle.addEventListener("click", () => {
+    themeController.switchTheme()
+})
