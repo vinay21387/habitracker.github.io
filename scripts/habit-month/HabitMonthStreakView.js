a559f47daf89240d7c4fd9682aeb953d85e75b2c
Vinay Kumar Malik <107244657+vinay21387@users.noreply.github.com>
2023-03-28 11:32:08 +0530

    Create HabitMonthStreakView.js

---
+++ scripts/habit-month/HabitMonthStreakView.js
@@ -0,0 +1,51 @@
+import {txtStreak} from '../utils/DOMelements.js';
+
+import {getTodayDay} from '../utils/dateUtils.js';
+
+import { HabitMonth } from './HabitMonth.js';
+
+export function createHabitMonthStreakView(){
+    var displayHabitMonth;
+
+    function update(habitMonth){
+
+        displayHabitMonth = habitMonth
+     
+        if(isNotCurrentMonth()) {
+            txtStreak.classList.add('invisible')
+            return
+        }
+    
+        let streak = 0
+        if(todayIsInNeutralState())
+            streak = calculateStreakFromYesterday()
+        else
+            streak = calculateStreakFromToday()
+    
+        if(streak <= 1){
+            txtStreak.classList.add('invisible')
+        }
+        else{
+            txtStreak.classList.remove('invisible')
+            txtStreak.innerText =  String(streak) + ' days Streak'
+        }
+    
+        
+        function isNotCurrentMonth(){
+            return displayHabitMonth.getId() != HabitMonth.generateTodaysId()
+        }
+        function todayIsInNeutralState(){
+            return displayHabitMonth.getDayAt(getTodayDay() - 1) == 0
+        }
+        
+        function calculateStreakFromToday(){
+            return habitMonth.getStreak(getTodayDay())
+        }
+        function calculateStreakFromYesterday(){
+            return habitMonth.getStreak(getTodayDay() - 1)
+        }
+    }
+    return {
+        update
+    }
+}
